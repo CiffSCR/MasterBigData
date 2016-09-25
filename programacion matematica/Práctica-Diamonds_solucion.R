eef66180853c0203e804783f4fbddb81468d254a
Sergio Castro <sergiocastro@campusciff.net>
2016-09-25 14:17:23 +0200

    practica final diamonds

--- programacion matematica/Práctica-Diamonds_solucion.R
+++ programacion matematica/Práctica-Diamonds_solucion.R
@@ -1,6 +1,7 @@
 # Cargamos los datos
 library(ggplot2) # los datos se encuentran en la librería ggplot2
 library(nortest)
+library(Rcmdr)
 dt<-as.data.frame(diamonds) #guardamos en un data frame los datos
 head(dt) # comprobamos que se han cargado
 attach(dt) # para poder utilizar las variables directamente
@@ -81,13 +82,13 @@ summary(dt2$z) #Sin estos valores, vemos que la distribución de valores es, mas
 hist(dt2$z)
 boxplot(dt2$z) # los graficos muestran que los datos tienen dos colas, con la derecha más larga. Tambien se ven algunos valores atípicos en la cola derecha
 
-#### Detecci󮠤e casos atcos y su tratamiento
+#### Detección de casos atípicos y su tratamiento
 #Hemos visto que todas las variables poseen valores atípicos. Una posible solución es eliminarlos para que no influyan en los resultados de los diferentes análisis. Por otra parte, como son muchos, datos los que se salen de los valores normales, quizás es recomendable dejarlos, puesto que, aunque sean un porcentaje pequeño, son valores que nos vamos a encontrar.
 #Voy a eliminar, al menos los que tienen valores nulos (0) en las variables "X", "Y" y "Z"
 dt<-dt[dt$x>0 & dt$y>0 & dt$z>0,]
 # Comprobamos la cantidad de outliers:
 out_carat=c(quantile(dt$carat,.25)-1.5*IQR(dt$carat), quantile(dt$carat,.75)+1.5*IQR(dt$carat))
-out_depth= c(quantile(dt$carat,.25)-1.5*IQR(dt$depth), quantile(dt$depth,.75)+1.5*IQR(dt$depth))
+out_depth= c(quantile(dt$depth,.25)-1.5*IQR(dt$depth), quantile(dt$depth,.75)+1.5*IQR(dt$depth))
 out_table= c(quantile(dt$table,.25)-1.5*IQR(dt$table), quantile(dt$table,.75)+1.5*IQR(dt$table))
 out_price= c(quantile(dt$price,.25)-1.5*IQR(dt$price), quantile(dt$price,.75)+1.5*IQR(dt$price))
 out_x = c(quantile(dt$x,.25)-1.5*IQR(dt$x), quantile(dt$x,.75)+1.5*IQR(dt$x))
@@ -140,7 +141,7 @@ precioVeryGood<-dt$price[dt$cut=="Very Good"]
 hist(precioGood)
 hist(precioVeryGood)
 # Ahora utilizamos el t.test para hacer el contrase entre las dos medias. El t.test es para datos que sigan una distribución normal, pero si el tamaño de la muestra es grande (mayor de 30) no es necesario que sigan dicha distribución:
-> t.test(precioGood, precioVeryGood,  alternative='two.sided')
+t.test(precioGood, precioVeryGood,  alternative='two.sided')
 
 #        Welch Two Sample t-test
 #
@@ -162,25 +163,27 @@ hist(precioVeryGood)
 
 #tenemos que pasar las variables categóricas a numéricas.
 #Obtenemos los distintos niveles de cada una de ellas y los reemplazamos por números. Obtenemos los niveles ordenados de menor a mayor (de peor a mejor)
-dt_recode<-dt[c('carat', 'depth', 'table', 'price', 'x', 'y', 'z')]
-dt_recode['numCut']<-as.numeric(dt$cut)
-dt_recode['numColor']<-as.numeric(dt$color)
-dt_recode['numClarity']<-as.numeric(dt$clarity)
+dt_recode<-dt
+dt_recode[,2]<-sapply(dt_recode[,2],switch,"Fair"=1,"Good"=2,"Very Good"=3,"Premium"=4,"Ideal"=5)
+dt_recode[,3]<-sapply(dt_recode[,3],switch,"J"=1,"I"=2,"H"=3,"G"=4,"'F'"=5,"E"=6,"D"=7)
+dt_recode[,4]<-sapply(dt_recode[,4],switch,"IF"=1,"VVS1"=2,"VVS2"=3,"VS1"=4,"VS2"=5,"SI1"=6,"SI2"=7,"I1"=8)
 
 head(dt_recode) #comprobamos que se han cambiado los valores
 plot(dt_recode) #vemos un scatterplot entre todas las variables, dos a dos.
-corrplot(cor(dt_recode), method="number") # nos muestra el coeficiente de correlación entre todas las variables.
-#                  carat        depth       table       price           x           y           z       numCut     numColor  numClarity
-#carat       1.000000000  0.006071982  0.18870386  0.92222112  0.98610576  0.98539472  0.98472104 -0.129388900  0.255570212 -0.35962317
-#depth       0.006071982  1.000000000 -0.30791702 -0.00738645 -0.04751424 -0.04799363  0.07047293 -0.133285953  0.043863218 -0.03556990
-#table       0.188703857 -0.307917021  1.00000000  0.13485058  0.19385046  0.18841622  0.15497986 -0.425495930  0.014545049 -0.16135977
-#price       0.922221117 -0.007386450  0.13485058  1.00000000  0.89780230  0.89910124  0.89574793 -0.066105007  0.139162243 -0.14359991
-#x           0.986105761 -0.047514241  0.19385046  0.89780230  1.00000000  0.99847548  0.99151621 -0.116509777  0.232836668 -0.37496155
-#y           0.985394719 -0.047993625  0.18841622  0.89910124  0.99847548  1.00000000  0.99150725 -0.120358894  0.232997879 -0.36986935
-#z           0.984721044  0.070472929  0.15497986  0.89574793  0.99151621  0.99150725  1.00000000 -0.133770990  0.237474806 -0.37539492
-#numCut     -0.129388900 -0.133285953 -0.42549593 -0.06610501 -0.11650978 -0.12035889 -0.13377099  1.000000000 -0.006563392  0.16607216
-#numColor    0.255570212  0.043863218  0.01454505  0.13916224  0.23283667  0.23299788  0.23747481 -0.006563392  1.000000000  0.05903221
-#numClarity -0.359623170 -0.035569899 -0.16135977 -0.14359991 -0.37496155 -0.36986935 -0.37539492  0.166072160  0.059032209  1.00000000
+cor(dt_recode) # nos muestra el coeficiente de correlación entre todas las variables.
+
+#              carat         cut       color     clarity        depth       table        price           x           y          z
+#carat    1.00000000 -0.12725901  0.25565398 -0.35830341  0.035242073  0.18682902  0.922784405  0.98653913  0.98574891  0.9853223
+#cut     -0.12725901  1.00000000 -0.01081142  0.16576169 -0.274081960 -0.40653369 -0.066394836 -0.10781531 -0.11172268 -0.1377845
+#color    0.25565398 -0.01081142  1.00000000  0.05947226  0.044450708  0.01751371  0.138672543  0.23356707  0.23371027  0.2369094
+#clarity -0.35830341  0.16576169  0.05947226  1.00000000 -0.066795105 -0.15740964 -0.144054306 -0.37264625 -0.36763744 -0.3749860
+#depth    0.03524207 -0.27408196  0.04445071 -0.06679510  1.000000000 -0.24059716  0.005624437 -0.01112742 -0.01277283  0.0922298
+#table    0.18682902 -0.40653369  0.01751371 -0.15740964 -0.240597157  1.00000000  0.136240038  0.18593231  0.18050787  0.1586113
+#price    0.92278441 -0.06639484  0.13867254 -0.14405431  0.005624437  0.13624004  1.000000000  0.89942474  0.90057949  0.8961437
+#x        0.98653913 -0.10781531  0.23356707 -0.37264625 -0.011127423  0.18593231  0.899424739  1.00000000  0.99853019  0.9933311
+#y        0.98574891 -0.11172268  0.23371027 -0.36763744 -0.012772834  0.18050787  0.900579487  0.99853019  1.00000000  0.9931567
+#z        0.98532227 -0.13778449  0.23690940 -0.37498598  0.092229796  0.15861132  0.896143738  0.99333108  0.99315669  1.0000000
+
 
 #vamos a considerar que una correlación mayor de 0.7 (positiva o negativa) es una correlación alta.
 # A la vista de la matriz de correlaciones, vemos que hay una fuerte correlacion positiva entre los pares de variables
@@ -188,5 +191,92 @@ corrplot(cor(dt_recode), method="number") # nos muestra el coeficiente de correl
 # es lógico pensar que las variables que indican el tamaño (x, y, z) están relacionadas con la variable peso (carat)
 # lo que observamos es que no hay corelación entre la calidad del corte, el color o la claridad y el precio.
 
+# Ahora, mediante un análisis de la varianza (ANOVA) vamos a ver si el precio depende de las distintas categorias de Color.
+# Para hacer un anova necesitamos que la variable respuesta (price) siga una distribución normal en cada categoría o bien, el cual es nuestro caso, que el tamaño de los datos sea grande. Tambien se necesita homogeniedad de la varainza en cada categoría. Esta última condicion la vamos a suponer para poder hacer el análisis.
+
+an_o_va <-aov(dt_recode$price~dt_recode$cut)
+
+summary(an_o_va)
+#                 Df    Sum Sq   Mean Sq F value Pr(>F)    
+#dt_recode$cut     1 1.574e+09 1.574e+09   210.2 <2e-16 ***
+#Residuals     47481 3.556e+11 7.489e+06                   
+#---
+#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+# El análisis de la varianza nos dice que hay diferencias significativas entre las medias de cada categoría.
 
 ######Análisis de regresión######
+
+#Analizamos los gráficos de correlacion y los coeficientes de corelación
+#tomamos el dataframe con lo valores recodificados para poder utilizar las variables categóricas como variables numéricas discretas. Esto se puede hacer porque todas las variables categóricas se pueden ordenar. Otra opción sería la de crear variables dummies de las distintas categorías. En nuestro caso se crearían demasiadas variables, puesto que hay muchas categorías.
+
+plot(dt_recode)
+partial.cor(dt_recode)
+
+#Sacamos la matriz de correlaciones parciales. Estas correlaciones entre variables no tienen en cuenta la interrelacion con el resto de variables:
+# como sólo nos interesa la relación con price, sólo pego esa fila:
+
+#              carat         cut       color     clarity        depth       table        price           x           y           z
+#price    0.60622390  0.06196948 -0.43503931  0.56725573  0.005237836 -0.04709215  1.000000000 -0.06873170  0.05150151 -0.04346383
+
+
+
+# Nos interesa la correlacion de la variable precio con el resto de variables, ya que va a ser nuestra variable respuesta.
+
+#Vemos que los valores más altos se encuentran en las variables carat, clarity (asociaciones positivas) y color (negativa)
+
+#Vamos a ir creando el model de forma manual con las variables, es decir, vamos a seguir el método stepwise de forma manual.
+
+# La máxima asociación con precio la vemos con la variable carat. Estudiamos ese modelo:
+
+model1 <- lm(dt_recode$price~dt_recode$carat) 
+summary(model1)
+resm1<-model1$residuals
+plot(resm1) # En los residuos vemos que no se distribuyen de forma aleatoria en torno al cero, por lo que el modelo no es bueno. Se ven dos zonas muy diferenciadas (alrededor del index 2100)
+summary(resm1)
+sd(resm1) # 1056.77
+
+#vamos a añadir la siguiente variable con más correlacion parcial, clarity
+model2 <- lm(dt_recode$price~dt_recode$carat+dt_recode$clarity) 
+summary(model2)
+resm2<-model2$residuals
+plot(resm2) #seguimos viendo las dos zonas en los residuos, pero en la sgunda parte ya se ve un comportamiento más aleatorio y centrado en el cero
+summary(resm2)
+sd(resm2) #903.5127
+#Con este model mejoramos el ajuste y disminuimos la varianza.
+
+#Vamos a añadir la variable color que es la siguiente con una mayor correlacion parcial:
+model3 <- lm(dt_recode$price~dt_recode$carat+dt_recode$clarity+dt_recode$color) 
+summary(model3)
+resm3<-model3$residuals
+plot(resm3) #seguimos viendo las dos zonas en los residuos, de la misma forma que en el caso anterior.
+summary(resm3)
+sd(resm3) 
+#disminuimos la varianza y mejoramos el ajuste, pero se sigue manteniendo la no aleatoriedad de los residuos.
+
+#Vamos a probar con una transformación logarítmica del precio para ver si de esta forma conseguimos un modelo mejor.
+
+caratLog<-log(dt_recode$carat)
+priceLog<-log(dt_recode$price)
+
+#Volvemos a hacer los modelos pero esta vez con la nueva variable como variable respuesta
+
+model1_log <- lm(priceLog~caratLog) 
+summary(model1_log)
+resm1_log<-model1_log$residuals
+plot(resm1_log) # El gráfico con los residuos muestra cierta falta de aleatoriedad pero menos que antes
+summary(resm1_log)
+sd(resm1_log) # 0.2483734
+
+#Este modelo con una sola variable tiene un ajuste bastante bueno (Adjusted R-squared:  0.9279)
+#Vamos a probar introduciendo una variable más para ver si mejoramos la distribución de los residuos
+clarityLog<-log(dt_recode$clarity)
+model2_log <- lm(priceLog~caratLog+clarityLog) 
+summary(model2_log)
+resm2_log<-model2_log$residuals
+plot(resm2_log) #seguimos viendo las dos zonas en los residuos, pero en la sgunda parte ya se ve un comportamiento más aleatorio y centrado en el cero
+summary(resm2_log)
+sd(resm2_log) #0.1839627
+
+#Este modelo tiene un ajuste muy bueno (Adjusted R-squared:  0.9605) y por el plot de residuos podemos suponer que éstos son aleatorios en torno al cero.
+ 
